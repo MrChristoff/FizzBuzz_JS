9206dd648842910b1081a6649fd3ee27e51ecf8e
Chris Lawrence <chris.lawrence@gmx.com>
2016-10-25 12:36:17 +0100

    logic and method tests passing

--- jasmine/spec/FizzBuzzSpec.js
+++ jasmine/spec/FizzBuzzSpec.js
@@ -1,9 +1,47 @@
 describe('FizzBuzz', function() {
-  var FIzzBuzz;
+  var fizzbuzz;
+  beforeEach(function() {
+    fizzbuzz = new FizzBuzz();
+  });
   describe('Knows when a number is', function() {
     it('divisible by 3', function() {
-      FizzBuzz = new FizzBuzz();
-      expect(isDivisibleByThree(3)).toBe(true);      
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+  describe('Knows when a number is NOT', function() {
+    it('divisible by 3', function() {
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+  describe('Knows when a number is', function() {
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+  describe('Knows when a number is NOT', function() {
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+    });
+  });
+  describe('Knows when a number is', function() {
+    it('divisible by both 3 and 5', function() {
+      expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+  });
+  describe('Knows when a number is NOT', function() {
+    it('divisible by either 3 or 5', function() {
+      expect(fizzbuzz.isDivisibleByThreeAndFive(7)).toBe(false);
+    });
+  });
+  describe('Returns Fizz, BuZZ, or FizzBuzz', function() {
+    it('Fizz', function() {
+      expect(fizzbuzz.fizzOrBuzz(3)).toBe("Fizz");
+    });
+    it('Buzz', function() {
+      expect(fizzbuzz.fizzOrBuzz(5)).toBe("Buzz");
+    });
+    it('FizzBuzz', function() {
+      expect(fizzbuzz.fizzOrBuzz(15)).toBe("FizzBuzz");
     });
   });
 });
--- jasmine/src/FizzBuzz.js
+++ jasmine/src/FizzBuzz.js
@@ -1,1 +1,23 @@
 var FizzBuzz = function(){};
+FizzBuzz.prototype.isDivisibleByThree = function (number) {
+  return (number % 3 === 0);
+};
+FizzBuzz.prototype.isDivisibleByFive = function (number) {
+  return (number % 5 === 0);
+};
+FizzBuzz.prototype.isDivisibleByThreeAndFive = function (number) {
+  // return (number % 3 === 0 && number % 5 === 0);
+  return (number % 15 === 0);
+};
+
+FizzBuzz.prototype.fizzOrBuzz = function (number) {
+  if (this.isDivisibleByThreeAndFive(number)) {
+    return "FizzBuzz"
+} else if (this.isDivisibleByThree(number)) {
+    return "Fizz"
+} else if (this.isDivisibleByFive(number)) {
+    return "Buzz"
+} else {
+    number
+}
+};
